6899f839c9870531e55b3b46ae92d5f61b6684b2
Shaban004 <mufasahussle@gmail.com>
2023-03-18 05:01:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,22 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+    char a;
+    int b;
+    long int c;
+    long long int d;
+    float e;
+
 printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of int: %ld byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float: %ld byte(s)\n", (unsigned long)sizeof(e));
+printf("Knowing the size of data types is important for optimizing memory usage and avoiding data loss in C programming. Use this code to check the sizes of common data types:
+Size of char: 1 byte(s)
+Size of int: 4 byte(s)
+Size of long int: 8 byte(s)
+Size of long long int: 8 byte(s)
+Size of float: 4 byte(s)");
 return (0);
 }
