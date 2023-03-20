6a3fa8d2387c1dc0ccbfd96e2c83ea748381ab6e
Shaban004 <mufasahussle@gmail.com>
2023-03-20 04:21:23 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,20 +8,20 @@
 int main(void)
 {
 int n;
-srand(time(NULL));
-int n = rand() - RAND_MAX / 2;
-printf("%d ", n);
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive\n");
+printf("%d is positive\n,n");
 }
 else if (n == 0)
 {
-printf("is zero\n");
+printf("%d is zero\n,n");
 }
 else
 {
-printf("is negative\n");
+printf("%d is negative\n,n");
 }
 return (0);
 {
