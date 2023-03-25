6592294022bc635417ea299a055029f8918fa605
Shaban004 <mufasahussle@gmail.com>
2023-03-25 15:43:06 -0700

    correcte file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 /**
  * print_sign - Prints sign of a number
  * @n: The number in which it's sign is to be printed
- * Return: Return 1 if number is greater then zero ,-1 if number is a negative and 0 if anything else
+ * Return: Return 1 if num is + ,-1 if num is - and 0 if anything else
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
 }
