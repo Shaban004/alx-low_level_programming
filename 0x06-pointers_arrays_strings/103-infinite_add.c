556f797b405a992541e1aafc162ac064bbe24062
Shaban004 <mufasahussle@gmail.com>
2023-03-30 12:16:24 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,18 +6,22 @@
  * @n2: Second number
  * @r: Buffer where the result will be stored
  * @size_r: Size of the buffer
- * Return: Pointer to the result or 0 if buffer is too small
+ * Returns: Pointer to the result or 0 if buffer is too small
+ * Return: on success,returns a pointer to the result. Otherwise, returns NULL.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int len1 = 0, len2 = 0, carry = 0, sum = 0, i, j;
 char *p;
+/* Calculate the lengths of n1 and n2 */
 while (n1[len1])
 len1++;
 while (n2[len2])
 len2++;
+/* Check if the buffer is too small */
 if (len1 >= size_r || len2 >= size_r)
 return (0);
+/* Add the digits of n1 and n2, starting from the end */
 i = len1 - 1;
 j = len2 - 1;
 r[size_r - 1] = '\0';
@@ -34,12 +38,14 @@ i--;
 j--;
 size_r--;
 }
+/* Handle any remaining carry */
 if (carry)
 {
 if (size_r <= 1)
 return (0);
 r[size_r - 2] = carry + '0';
 }
+/* Remove leading zeros from the result */
 p = r;
 while (*p == '0' && *(p + 1) != '\0')
 p++;
