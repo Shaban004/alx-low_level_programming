9b00331ee90edca416b86b79f479587899c3858d
Shaban004 <mufasahussle@gmail.com>
2023-03-30 06:14:24 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -6,7 +11,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-*p = 98; /* <-- new line of code */
+*(p + 5) = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
