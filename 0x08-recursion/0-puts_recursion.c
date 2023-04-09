37f20b099acd4331d9481c60091dad194aff09a8
Shaban004 <mufasahussle@gmail.com>
2023-04-09 14:21:33 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-putchar(*s);
-puts_recursion(s + 1);
-}
-else
-putchar('\n');
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		putchar('\n');
 }
